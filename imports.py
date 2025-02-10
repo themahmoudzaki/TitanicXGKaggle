5956ee939db1111cee6a6ea9774f402bd30f7fbd
themahmoudzaki <Mahmoudzakylinkedin@gmail.com>
2025-02-10 23:40:41 +0200

    Model currently has a 0.773 Accuracy

--- imports.py
+++ imports.py
@@ -5,6 +5,7 @@ import sys
 import sklearn
 from sklearn.model_selection    import train_test_split
 from sklearn.model_selection    import GridSearchCV
+from sklearn.metrics            import classification_report
 from xgboost                    import XGBClassifier
 from preprocess_data            import preprocess_data
 from xgboost_model              import xgboost_model
--- xgboost_model.py
+++ xgboost_model.py
@@ -4,11 +4,15 @@ def xgboost_model(tt_files, save_folder):
     train_X, test_X, train_y, test_y = tt_files
 
     grid_params = {
-        'n_estimators': 2,
-        'max_depth':    5,
-        'learning_rate': 1e-3,
+        'n_estimators': 5,
+        'max_depth':    10,
+        'learning_rate': 1e-1,
         'objective': 'binary:logistic'
     }
     model = XGBClassifier(**grid_params)
     model.fit(train_X, train_y)
+    y_hat = model.predict(test_X)
+    classification = classification_report(test_y, y_hat)
+    print(f'Test classification: \n{classification}')
+
     return model
